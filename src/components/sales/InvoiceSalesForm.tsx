4efd501329543d54d4200f134b5fb99e9f2cea92
Ademir <ademir@debian.local>
2023-09-22 14:50:27 -0300

    settings - layout invoice_sales

--- src/components/sales/InvoiceSalesForm.tsx
+++ src/components/sales/InvoiceSalesForm.tsx
@@ -23,18 +23,22 @@ export function InvoiceSalesForm({
   return (
     <div className="container-global" >
       <div className="main-global">
-        <form className='main-global-form'>
-          <h1>Faturar o pedido.</h1><hr></hr>
-          <label>{alert}</label>
-          <label>{message}</label><br />
-          <strong>Dados da entrega e emissão da nota :</strong>
+        <div className='main-global-form'>
+           <img className="tp-info" src="/img/tp_info.ico"></img>
+           <span> Direct Fatura - CENTRO INFORMÁTICA</span>
+          <h4 className='text-center'>FATURAR O PEDIDO</h4>
+           {/* <label>{alert}</label> */}
+           {/* <label>{message}</label> */}
+          <label className='text-center'>( DADOS PARA ENTREGA E EMISSÃO DA NOTA !! )</label>
           <div><strong>Nome do cliente</strong><span>{children.name_pers}</span></div>
           <div><strong>Contato do cliente</strong><span>{children.phone_pers}</span></div>
           <div><strong>CPF do cliente</strong><span>{children.cpf_pers}</span></div>
-          <div><strong>Endereço do cliente</strong><span>{children.address_pers}</span></div><br />
-          <button className="btn btn-primary"><a href={'/person_update'}>Atualizar cadastro</a></button>
-          <hr />
-          <strong>Valores na nota :</strong>
+          <div><strong>Endereço do cliente</strong><span>{children.address_pers}</span></div>
+          
+          <a href='/person_update'>Matenha seus dados cadastrais atualizados. <b>clique-aqui !</b></a>
+
+          <hr></hr>
+          <strong>VALORES NA NOTA</strong>
           <div><strong>SubTotal</strong><span>{currencyFormat(children.tItens)}</span></div>
           <div><strong>Cupom de desconto</strong><span>{currencyFormat(children.disc_sale)}</span></div>
           <div><strong>Total da nota</strong><span>{currencyFormat(children.tNote)}</span></div>
@@ -59,7 +63,7 @@ export function InvoiceSalesForm({
           <button onClick={handleSubmit}>Próximo passo: Pagamento</button>
           <a href='###'>{'Mantenha seu cadastro atualizado'}</a>
           <span className='load-list-itens' >{loadItens}</span>
-        </form>
+        </div>
       </div>
     </div>
   )
