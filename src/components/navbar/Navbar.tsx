4e5074be43774d2480ca3cf7d855fbdd54f82414
Ademir <ademir@debian.local>
2023-10-08 10:05:05 -0300

    refactored navbar menus and links

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,10 +2,8 @@ import { checksUserLogged, checkAdminPrivilege } from "../utils/checksUserLogged
 import { Globais } from "../globais/Globais";
 import { Logo } from "../logo/Logo";
 
-
 import "../assets/dist/css/bootstrap.min.css"
 import "./Navbar.css"
-
 
 export function NavBar() {
 
@@ -14,14 +12,14 @@ export function NavBar() {
 
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-dark" style={{ background: 'rgb(172, 170, 170)', border: '1px solid white' }} aria-label="Offcanvas navbar large">
+            <nav className="navbar navbar-expand-lg navbar-dark" style={{ background: 'rgb(237,237,237', border: '1px solid rgb(237,237,237)' }} aria-label="Offcanvas navbar large">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="/dashboardefault">{<Logo />}</a>
                     <button className="navbar-toggler" style={{ background: 'gray', border: 'none' }} type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar2"
                         aria-controls="offcanvasNavbar2">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="offcanvas offcanvas-end bg-secondary" id="offcanvasNavbar2" aria-labelledby="offcanvasNavbar2Label">
+                    <div className="offcanvas offcanvas-end bg-secondary-old" style={{ background: 'rgb(237, 237, 237)', border: '3px solid gray', padding: '0px' }} id="offcanvasNavbar2" aria-labelledby="offcanvasNavbar2Label">
                         <div className="offcanvas-header">
                             <h5 className="offcanvas-title" id="offcanvasNavbar2Label"><a href={Globais.URL_CENTROINFO + '/contact'}>Fale conosco</a></h5>
                             <button type="button" className="btn-close btn-close-dark" data-bs-dismiss="offcanvas"
@@ -56,7 +54,7 @@ export function NavBar() {
                                 <li className="nav-item dropdown">
                                     <a className="nav-link dropdown-toggle" href="##" role="button" data-bs-toggle="dropdown"
                                         aria-expanded="false">Cadastros</a>
-                                    <ul className="dropdown-menu" style={{ background: 'rgb(172, 170, 170)', border: '2px solid white', padding: '3px' }}>
+                                    <ul className="dropdown-menu" style={{ background: 'rgb(237,237,237)', border: '1px solid gray', padding: '12px' }}>
                                         <li><a className="dropdown-item nav-link" href="/form_person">Pessoas - Comprador</a></li>
 
                                         <li>{checkAdminPrivilege() === privilAdmin ? <a className="dropdown-item nav-link" href="/form_product">Produtos - Admin</a> :
@@ -72,7 +70,7 @@ export function NavBar() {
                                 </li>
                                 <li className="nav-item dropdown">
                                     <a className="nav-link dropdown-toggle" href="##" role="button" data-bs-toggle="dropdown" aria-expanded="false">Listagem</a>
-                                    <ul className="dropdown-menu" style={{ background: 'rgb(172, 170, 170)', border: '2px solid white', padding: '3px' }}>
+                                    <ul className="dropdown-menu" style={{ background: 'rgb(237, 237, 237)', border: '1px solid gray', padding: '12px' }}>
                                         <li><a className="dropdown-item nav-link" href="/person_list">Pessoas</a></li>
                                         <li><a className="dropdown-item nav-link" href="/product_list">Produtos</a></li>
                                         <li>
