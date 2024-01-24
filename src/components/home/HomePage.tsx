533723b9518b9a5a360308907de372408f5abd1c
Ademir <ademir@debian.local>
2024-01-24 18:19:18 -0300

    up code

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -2,35 +2,46 @@ import './HomePage.css'
 
 type Props = {
     id?: number;
-    item_img:string;
+    item_img: string;
     item?: number;
-    descric: string | number | "" | 0 ;
-    sector: number | string |undefined;
+    descric: string | number | "" | 0;
+    sector: number | string | undefined;
     brand: number | string | undefined;
     amount: number | undefined;
     valor: number | string | "money";
     tItem?: number | string | "money";
-    incrementAmount:any;
+    incrementAmount?: any;
     addItem: any;
-    decrementAmount:any;
+    decrementAmount?: any;
+    selectAmount: any;
+    inputAmout?: any;
 }
 
-export function ListItens(props:Props){
-    return(
-    <div className='container-itens'>
-        <div className='main-itens'>
-        <img className='itens-img' src={props.item_img} alt="Aguardando Item !!"></img>
-            <p><b>ITEM</b> {props.id}</p>
-            <p className='text-center'>{props.descric}</p>
-            {props.amount}
-            <span><b>Marca: </b>{props.brand}</span>
-            <span><b>Setor: </b>{props.sector}</span>
-            <p><b>R$</b> {props.valor}</p>
-            <>{props.addItem}</>
-            <>{props.decrementAmount}</>
-            <>/</>
-            <>{props.incrementAmount}</>
+export function ListItens(props: Props) {
+    return (
+        <div className='container-itens'>
+            <div className='main-itens'>
+                <img className='itens-img' src={props.item_img} alt="Aguardando Item !!"></img>
+                <p><b>ITEM</b> {props.id}</p>
+                <p className='text-center'>{props.descric}</p>
+                {props.amount}
+                <span><b>Marca: </b>{props.brand}</span>
+                <span><b>Setor: </b>{props.sector}</span>
+                <p><b>R$</b> {props.valor}</p>
+                <label>Quantidade</label>
+                <div className='select-amount'>{props.selectAmount}</div>
+
+                {/* <>{props.decrementAmount}</>
+                    <>/</>
+                    <>{props.incrementAmount}</>
+                    <>{props.selectAmount}</> */}
+
+                {/* <label>Informe a quantidade</label>
+                    <div className='input-amount'>{props.inputAmout}</div> */}
+
+                <>{props.addItem}</>
+
+            </div>
         </div>
-    </div>
     )
 }
--- src/useCases/home/HomePage.tsx
+++ src/useCases/home/HomePage.tsx
@@ -142,16 +142,16 @@ export function HomePage() {
         }
     }
 
-    function handleAmountIncrement() {
-        amount++
-        setAmount(amount);
-    }
-    function handleAmountDecrement() {
-        if (amount > 1) {
-            amount--
-            setAmount(amount);
-        }
-    }
+    // function handleAmountIncrement() {
+    // amount++
+    // setAmount(amount);
+    // }
+    // function handleAmountDecrement() {
+    // if (amount > 1) {
+    // amount--
+    // setAmount(amount);
+    // }
+    // }
 
     async function getBrands() {
         try {
@@ -229,14 +229,52 @@ export function HomePage() {
                     amount={item.amount}
                     valor={item.val_max_product}
 
+                    selectAmount={<select onChange={e => setAmount(parseInt(e.target.value))}>
+                        <option>{1}</option>
+                        <option>{2}</option>
+                        <option>{3}</option>
+                        <option>{4}</option>
+                        <option>{5}</option>
+                        <option>{6}</option>
+                        <option>{7}</option>
+                        <option>{8}</option>
+                        <option>{9}</option>
+                        <option>{10}</option>
+                        <option>{11}</option>
+                        <option>{12}</option>
+                        <option>{13}</option>
+                        <option>{14}</option>
+                        <option>{15}</option>
+                        <option>{16}</option>
+                        <option>{17}</option>
+                        <option>{18}</option>
+                        <option>{19}</option>
+                        <option>{20}</option>
+                        <option>{21}</option>
+                        <option>{22}</option>
+                        <option>{23}</option>
+                        <option>{24}</option>
+                        <option>{25}</option>
+                        <option>{26}</option>
+                        <option>{27}</option>
+                        <option>{28}</option>
+                        <option>{29}</option>
+                        <option>{30}</option>
+                    </select>}
+
+                    // decrementAmount={<button style={{ display: 'contents', color: 'gray', fontSize: '14px' }} onClick={() =>
+                    //     handleAmountDecrement()}>{'Diminuir'}</button>}
+
+                    // incrementAmount={<button style={{ display: 'contents', color: 'blue', fontSize: '14px' }} onClick={() =>
+                    //     handleAmountIncrement()}>{'Aumentar'}</button>}
+
+                    // inputAmout={<input
+                    //     onChange={e => setAmount(parseInt(e.target.value))}
+                    //     placeholder='Informe a quantidade'
+                    // />}
+
                     addItem={<button className='btn btn-primary' onClick={() =>
-                        handleItem(item)}>Comprar {amount} Item</button>}
-
-                    decrementAmount={<button style={{ display: 'contents', color: 'gray', fontSize: '14px'}} onClick={() =>
-                        handleAmountDecrement()}>{'Diminuir'}</button>} 
-
-                    incrementAmount={<button style={{ display: 'contents', color: 'blue', fontSize: '14px' }} onClick={() =>
-                        handleAmountIncrement()}>{'Aumentar'}</button>}
+                        handleItem(item)}>Comprar</button>}
                 />
             )))}
             <Whats />
